3b75fd61cb9bea31713051a82dd3b412b59e3c18
Mckayla Kao <93305848+mckaylakao@users.noreply.github.com>
2026-05-14 09:57:07 -0700

    Initialize main.py with unittest setup

---
+++ main.py
@@ -0,0 +1,12 @@
+from typing import *
+from dataclasses import dataclass
+import unittest
+import sys
+import string
+sys.setrecursionlimit(10**6)
+
+class Tests(unittest.TestCase):
+  pass
+
+if(__name__ == '__main__'):
+  unittest.main()
